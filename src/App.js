d8dc8c409307deb826167710d8f770790924b0a9
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-02-06 17:32:41 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import {useState} from 'react';
+// import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route,Routes,Navigate } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
@@ -8,6 +10,10 @@ import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 
 function App() {
+  /**
+   * including the old version 
+   */
+  const [cartIsEmpty] = useState(true);
   return (
     <div className="App">
       <BrowserRouter>
@@ -17,18 +23,39 @@ function App() {
           <Link to="/about">About</Link>
           <Link to="/products">Products</Link>
         </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
+        {/* <Switch> */}
+        <Routes>
+          {/* <Route exact path="/" component={Home} /> */}
+          {/* <Route path="/" component={Home} /> */}
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about">
             <About />
-          </Route>
-          <Route path="/products/:id">
+          </Route> */}
+           <Route path="/about" element={<About />} />
+          {/* <Route path="/products/:id">
             <ProductDetails />
-          </Route>
-          <Route path="/products">
+          </Route> */}
+          <Route path="/products/:id" element={<ProductDetails />} />
+          {/* <Route path="/products">
             <Products />
-          </Route>
-        </Switch>
+          </Route> */}
+          <Route path="/products" element={ <Products />} />
+          {/* we could inline some jsx template inside the element props, insted referencing these other droop in page components */}
+          {/* example: look at test route */}
+          <Route path="/test" element={(
+               <div>
+                    <h2>test page..</h2>
+               </div>
+          )}/>
+          {/* an OLD EXAMPLE */}
+          {/* <Route path="/redirect" element={<Rdirect to="/about" />}/> */}
+          <Route path="/redirect" element={<Navigate to="/about" />}/> 
+          <Route
+           path="/checkout"
+           element={cartIsEmpty ? <Navigate to="/products"/> : <p>checkout</p> }
+           />
+        {/* </Switch> */}
+        </Routes>
       </BrowserRouter>
     </div>
   )
